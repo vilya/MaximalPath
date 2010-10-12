b9367e63c0ad4952a573f6f350d47c1e96cf7ccc
Vilya Harvey <vilya.harvey@gmail.com>
2010-10-12 01:58:25 +0100

    Initial commit

---
+++ maximalpath.py
@@ -0,0 +1,106 @@
+import sys
+import time
+
+
+class Graph(object):
+  def __init__(self):
+    self.nodes = set([])
+    self.edges = {}
+
+
+  def addEdge(self, fromNode, toNode):
+    fromNode = fromNode.lower()
+    toNode = toNode.lower()
+
+    self.nodes.add(fromNode)
+    self.nodes.add(toNode)
+
+    if fromNode not in self.edges:
+      self.edges[fromNode] = set([toNode])
+    else:
+      self.edges[fromNode].add(toNode)
+
+    if toNode not in self.edges:
+      self.edges[toNode] = set([fromNode])
+    else:
+      self.edges[toNode].add(fromNode)
+
+
+  def parse(self, f):
+    for line in f:
+      edge = line.strip()
+      fromNode = edge[:3]
+      toNode = edge[3:]
+      self.addEdge(fromNode, toNode)
+
+
+  def printDot(self, f):
+    print >> f, "graph Sample {"
+    for fromNode in self.nodes:
+      if fromNode not in self.edges:
+        continue
+      for toNode in self.edges[fromNode]:
+        print >> f, "  %s -- %s;" % (fromNode, toNode)
+    print >> f, "}"
+
+
+
+def MaximalPath(graph, start, num, path=[], count=0):
+  try:
+    if path == []:
+      start = start.lower()
+      return MaximalPath(graph, start, num, [start], count)
+
+    if path[-1] in graph.edges:
+      nextNodes = sorted([n for n in graph.edges[path[-1]] if n not in path])
+      if nextNodes != []:
+        newCount = 0
+        try:
+          for n in nextNodes:
+            newCount += MaximalPath(graph, start, num, path + [n], count + newCount)
+          return newCount
+        except KeyboardInterrupt:
+          print "\n%d paths found so far" % (count + newCount)
+          raise Exception("Aborted")
+
+    if count < num:
+      print " -> ".join(path)
+
+    return 1
+  except KeyboardInterrupt:
+    print "\n%d paths found so far" % count
+    raise Exception("Aborted")
+
+
+    
+def main():
+  if len(sys.argv) != 3:
+    print >> sys.stderr, "Usage: %s <graph> <nodes>" % sys.argv[0]
+    sys.exit(-1)
+
+  f = open(sys.argv[1])
+  graph = Graph()
+  graph.parse(f)
+  f.close()
+
+  f = open(sys.argv[2])
+  num = int(f.next().strip())
+  for line in f:
+    start = line.strip()
+    print "First %d lexicographic paths from %s:" % (num, start)
+    count = MaximalPath(graph, start, num)
+    print "Total maximal paths starting from %s: %d" % (start, count)
+    print ""
+
+  #graph.printDot(sys.stdout)
+
+
+if __name__ == '__main__':
+  begin = time.clock()
+  try:
+    main()
+  except Exception, e:
+    print str(e)
+  end = time.clock()
+  print >> sys.stderr, "Execution completed in %5.4f seconds" % (end - begin)
+
---
+++ tools/randomgraph.py
@@ -0,0 +1,68 @@
+import random, sys
+
+def MakeNodes(numNodes):
+  letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  nodes = set([])
+  while len(nodes) < numNodes:
+    node = random.choice(letters) + random.choice(letters) + random.choice(letters)
+    nodes.add(node)
+  nodes = tuple(nodes)
+  return nodes
+
+
+def MakeEdges(nodes, numEdges):
+  edges = {}
+  while numEdges > 0:
+    fromNode = random.choice(nodes)
+    toNode = random.choice(nodes)
+    if fromNode not in edges:
+      edges[fromNode] = set([toNode])
+      numEdges -= 1
+    elif toNode not in edges[fromNode]:
+      edges[fromNode].add(toNode)
+      numEdges -= 1
+  return edges
+
+
+def PrintGraph(edges, filename):
+  with open(filename, "w") as f:
+    for fromNode, toNodes in edges.iteritems():
+      for toNode in toNodes:
+        print >> f, fromNode + toNode
+
+
+def PrintNodes(nodes, numPaths, numStarts, filename):
+  starts = set([])
+  while len(starts) < numStarts:
+    starts.add(random.choice(nodes))
+
+  with open(filename, "w") as f:
+    print >> f, numStarts
+    for n in starts:
+      print >> f, n
+
+
+def main():
+  if len(sys.argv) != 6:
+    print >> sys.stderr, "Usage: %s <nodes> <edges> <paths> <starts> <basename>" % sys.argv[0]
+    sys.exit(-1)
+
+  numNodes = int(sys.argv[1])
+  numEdges = int(sys.argv[2])
+  numPaths = int(sys.argv[3])
+  numStarts = int(sys.argv[4])
+  graphName = "%s-graph.txt" % sys.argv[5]
+  nodesName = "%s-nodes.txt" % sys.argv[5]
+
+  if numStarts > numNodes:
+    raise Exception("You ask too much dahlink!")
+
+  nodes = MakeNodes(numNodes)
+  edges = MakeEdges(nodes, numEdges)
+  PrintGraph(edges, graphName)
+  PrintNodes(nodes, numPaths, numStarts, nodesName)
+
+
+if __name__ == '__main__':
+  main()
+
