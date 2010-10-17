8bbc3b1aeb4f8113cf8a1f17ad3f3809925aa3d2
Vilya Harvey <vilya.harvey@gmail.com>
2010-10-17 15:24:22 +0100

    Made all the python scripts executable.

--- tools/graphstats.py
+++ tools/graphstats.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import sys
 
 def main():
@@ -7,11 +8,16 @@ def main():
   
   graph = {}
   edgeCount = 0
+  duplicates = 0
   with open(sys.argv[1]) as f:
     for line in f:
       line = line.upper()
       fromNode = line[:3]
       toNode = line[3:6]
+
+      if fromNode in graph and toNode in graph[fromNode]:
+        duplicates += 1
+        print line,
 
       if fromNode not in graph:
         graph[fromNode] = [toNode]
@@ -32,6 +38,7 @@ def main():
     max([len(edges) for _, edges in graph.iteritems()])
   )
   print "%d self edges" % sum([1 for node, edges in graph.iteritems() if node in edges])
+  print "%d duplicate edges" % duplicates
 
 
 if __name__ == '__main__':
